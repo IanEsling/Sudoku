74e5725fa941a823e760d44566fd4a3c253827bc
Ian <ian.esling@gmail.com>
2012-06-03 17:23:25 +0100

    solving cells with naked pairs

--- src/sudoku_clj/solvers/naked_pairs_solver.clj
+++ src/sudoku_clj/solvers/naked_pairs_solver.clj
@@ -6,4 +6,11 @@
 
 (defn remove-naked-pairs-in-unit
   [unit]
-  unit)
+  (loop [pairs (keys (filter #(= 2 (count (val %))) (group-by #(val %) unit)))
+         new-unit unit]
+    (if-let [pair (first pairs)]
+      (recur (next pairs)
+        (reduce
+          #(into %1 {(key %2) (if-not (= (val %2) pair) (difference (val %2) pair) (val %2))}) {} new-unit))
+      new-unit)
+    ))
--- test/sudoku_clj/test/solvers/naked_pairs.clj
+++ test/sudoku_clj/test/solvers/naked_pairs.clj
@@ -13,3 +13,37 @@
   (get-cell-numbers 1 8 (remove-naked-pairs-in-unit (get-row-number 1 board))) => #{7 8}
   (get-cell-numbers 1 9 (remove-naked-pairs-in-unit (get-row-number 1 board))) => #{9}
   )
+
+(fact "naked pairs will have their numbers removed from the numbers for all cells in the same region"
+  (def partially-solved {[1 3] #{1 2} [2 3] #{1 2} [3 3] #{1 2 3}})
+  (def board (update-cells-in-board (create-board [9 6 0 0 0 0 0 0 0
+                                                   8 5 0 0 0 0 0 0 0
+                                                   7 4 0 0 0 0 0 0 0
+                                                   6 0 0 0 0 0 0 0 0
+                                                   5 0 0 0 0 0 0 0 0
+                                                   4 0 0 0 0 0 0 0 0
+                                                   0 0 0 0 0 0 0 0 0
+                                                   0 0 0 0 0 0 0 0 0
+                                                   0 0 0 0 0 0 0 0 0
+                                                   ]) partially-solved))
+  (get-cell-numbers 1 3 (remove-naked-pairs-in-unit (get-region-number 1 board))) => #{1 2}
+  (get-cell-numbers 2 3 (remove-naked-pairs-in-unit (get-region-number 1 board))) => #{1 2}
+  (get-cell-numbers 3 3 (remove-naked-pairs-in-unit (get-region-number 1 board))) => #{3}
+  )
+
+(fact "naked pairs will have their numbers removed from the numbers for all cells in the same column"
+  (def partially-solved {[7 1] #{1 2} [8 1] #{1 2} [9 1] #{1 2 3}})
+  (def board (update-cells-in-board (create-board [9 0 0 0 0 0 0 0 0
+                                                   8 0 0 0 0 0 0 0 0
+                                                   7 0 0 0 0 0 0 0 0
+                                                   6 0 0 0 0 0 0 0 0
+                                                   5 0 0 0 0 0 0 0 0
+                                                   4 0 0 0 0 0 0 0 0
+                                                   0 0 0 0 0 0 0 0 0
+                                                   0 0 0 0 0 0 0 0 0
+                                                   0 0 0 0 0 0 0 0 0
+                                                   ]) partially-solved))
+  (get-cell-numbers 7 1 (remove-naked-pairs-in-unit (get-column-number 1 board))) => #{1 2}
+  (get-cell-numbers 8 1 (remove-naked-pairs-in-unit (get-column-number 1 board))) => #{1 2}
+  (get-cell-numbers 9 1 (remove-naked-pairs-in-unit (get-column-number 1 board))) => #{3}
+  )
